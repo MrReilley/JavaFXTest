7016f749e133d5cd30077981517d70475a4f9ccb
Dale Reilley <50684808+MrReilley@users.noreply.github.com>
2022-04-12 13:44:03 -0700

    Add files via upload

---
+++ JavaFX Application/src/module-info.java
@@ -0,0 +1,10 @@
+module JavaFX.Application {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+    requires java.datatransfer;
+    requires javafx.web;
+
+
+    opens sample;
+}
